161de9e872d0fdd4d6dbef04d7344aeae147e298
KyNarec <jim48@gmx.de>
2025-06-08 16:08:38 +0200

    Notification is now clickable and supports One UI 7. Added Visual feedback for some buttons

--- app/src/main/java/com/kynarec/kmusic/service/MediaNotificationManager.kt
+++ app/src/main/java/com/kynarec/kmusic/service/MediaNotificationManager.kt
@@ -1,12 +1,15 @@
 package com.kynarec.kmusic.service
 
 import android.app.Notification
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import androidx.core.app.NotificationCompat
 import androidx.media.app.NotificationCompat.MediaStyle
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
+import com.kynarec.kmusic.MainActivity
 import com.kynarec.kmusic.R
 
 class MediaNotificationManager(
@@ -22,6 +25,15 @@ class MediaNotificationManager(
     private val playbackState = mediaSession.controller.playbackState
     private val isPlaying = playbackState?.state == PlaybackStateCompat.STATE_PLAYING
     private val isPaused = playbackState?.state == PlaybackStateCompat.STATE_PAUSED
+
+    // Used for redirecting to the app after Notification is clicked
+    private val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP
+    }
+    private val pendingIntent = PendingIntent.getActivity(
+        context, 0, intent,
+        PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
 
     fun buildNotification(
@@ -53,6 +65,7 @@ class MediaNotificationManager(
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             .setOnlyAlertOnce(true)
             .setOngoing(isPlaying)
+            .setContentIntent(pendingIntent)
             .build()
     }
 
