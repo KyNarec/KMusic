9cc0dcd6125178fe3a8bd766f1878f79024264ec
KyNarec <113847208+KyNarec@users.noreply.github.com>
2026-04-13 14:50:13 +0200

    Revert "Revert "feat: add Lottie support and enhance player animations""

--- app/src/main/kotlin/com/kynarec/kmusic/ui/components/player/QueueSongComponent.kt
+++ app/src/main/kotlin/com/kynarec/kmusic/ui/components/player/QueueSongComponent.kt
@@ -10,34 +10,52 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Equalizer
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
 import coil.imageLoader
+import com.airbnb.lottie.LottieProperty
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.airbnb.lottie.compose.rememberLottieDynamicProperties
+import com.airbnb.lottie.compose.rememberLottieDynamicProperty
+import com.kynarec.kmusic.R
 import com.kynarec.kmusic.data.db.entities.Song
+import com.kynarec.kmusic.data.db.entities.SongArtist
 import com.kynarec.kmusic.ui.components.MarqueeBox
+import com.kynarec.kmusic.ui.theme.KMusicTheme
 import sh.calvin.reorderable.ReorderableCollectionItemScope
+import sh.calvin.reorderable.ReorderableItem
+import sh.calvin.reorderable.rememberReorderableLazyListState
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -124,10 +142,29 @@ fun QueueSongComponent(
                     horizontalAlignment = Alignment.End
                 ) {
                     if (isPlaying) {
-                        Icon(
-                            Icons.Default.Equalizer,
-                            contentDescription = "Equalizer",
+                        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.playing))
+                        val progress by animateLottieCompositionAsState(
+                            composition,
+                            iterations = LottieConstants.IterateForever,
+                            speed = 0.5f
                         )
+                        val dynamicProperties = rememberLottieDynamicProperties(
+                            rememberLottieDynamicProperty(
+                                property = LottieProperty.COLOR,
+                                value = MaterialTheme.colorScheme.onSecondaryContainer.toArgb(),
+                                keyPath = arrayOf("**")
+                            )
+                        )
+                        LottieAnimation(
+                            composition = composition,
+                            progress = { progress },
+                            dynamicProperties = dynamicProperties,
+                            modifier = Modifier.size(34.dp)
+                        )
+//                        Icon(
+//                            Icons.Default.Equalizer,
+//                            contentDescription = "Equalizer",
+//                        )
                     }
                     Spacer(modifier = Modifier.weight(1f))
 
@@ -135,7 +172,7 @@ fun QueueSongComponent(
                         text = duration,
                         fontSize = 14.sp,
                         modifier = Modifier
-                            .padding(bottom = 4.dp)
+//                            .padding(bottom = 4.dp)
                     )
                 }
             }
@@ -166,6 +203,55 @@ fun QueueSongComponent(
                     )
                 }
 
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Preview
+@Composable
+fun QueueSongComponentPreview() {
+    KMusicTheme(
+        darkTheme = true,
+        dynamicColor = false
+    ) {
+        Scaffold(Modifier.fillMaxSize()) { innerPadding ->
+            Column(Modifier.padding(innerPadding)) {
+                val lazyListState = rememberLazyListState()
+                val reorderableLazyListState =
+                    rememberReorderableLazyListState(lazyListState) { from, to ->
+
+                    }
+                val song = Song(
+                    id = "",
+                    title = "Numb",
+                    artists = listOf(SongArtist("", "LinkinPark")),
+                    albumId = "",
+                    duration = "3:07",
+                    thumbnail = "",
+                    likedAt = 0,
+                    totalPlayTimeMs = 1
+                )
+                LazyColumn(
+                    Modifier
+                        .fillMaxWidth()
+                ) {
+                    item {
+                        ReorderableItem(reorderableLazyListState, key = song.id) { isDragging ->
+                            QueueSongComponent(
+                                song = song,
+                                onClick = {},
+                                onLongClick = {},
+                                isPlaying = true,
+                                onDragStarted = {},
+                                onDragStopped = {},
+                                reorderableCollectionItemScope = this@ReorderableItem,
+                                draggingEnabled = true,
+                            )
+                        }
+                    }
+                }
             }
         }
     }
